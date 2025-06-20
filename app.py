8371f32f00c4cfe935134681019282ffd932eb45
Fredo Boss <apvitangcor@gmail.com>
2025-06-20 10:17:59 +0800

    text detected

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("hello bay")
+import easyocr
+import matplotlib.pyplot as plt
+import cv2
+
+
+# print("hello bay")
+# read image
+img_path = 'img/1.png'
+
+img = cv2.imread(img_path)
+# text detector
+reader = easyocr.Reader(['en'], gpu=True)
+# detect text in img
+text = reader.readtext(img)
+
+print(text)
+# draw bbox
