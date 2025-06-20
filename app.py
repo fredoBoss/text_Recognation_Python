9c6c448e7f289ee4e574e6230e463f8b2bb373d8
Fredo Boss <apvitangcor@gmail.com>
2025-06-20 10:33:06 +0800

    updated code for detection

--- app.py
+++ app.py
@@ -9,9 +9,17 @@ img_path = 'img/1.png'
 
 img = cv2.imread(img_path)
 # text detector
-reader = easyocr.Reader(['en'], gpu=True)
+reader = easyocr.Reader(['en'], gpu=False)
 # detect text in img
 text = reader.readtext(img)
 
-print(text)
-# draw bbox
+for t in text:
+    print(t)
+    
+    bbox, txt, score = t
+    
+    cv2.rectangle(img, bbox[0], bbox[2], (0, 255, 0), 5)
+    
+    
+plt.imshow(img)
+plt.show
